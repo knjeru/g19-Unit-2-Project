94005841747a71c55f777598f565e3ac81f97888
Garan Guillory <garanguillory@gmail.com>
2016-03-14 16:25:23 -0600

    Complete vet routes, queries, and tests. Edit vets seed file to include website.

--- unit-2/src/server/test/petTest.js
+++ unit-2/src/server/test/petTest.js
@@ -25,11 +25,63 @@ describe('Pet API routes', function() {
       });
   });
 
+  describe('Add a single pet', function() {
+      it('should add a single pet into the db', function(done){
+        chai.request(server)
+        .post('/api/pets/new')
+        .send({
+                name: 'Archibald',
+                type: 'Dog',
+                breed: 'Pomeranian',
+                picture_url: 'https://upload.wikimedia.org/wikipedia/commons/c/ca/Pomeranian.JPG',
+                weight: '8',
+                allergies: 'Everything',
+                illnesses: 'Everyting',
+                last_vet_visit: '2016-03-12T00:00:00.000Z',
+                tendencies: 'Is tiny',
+                favorites: 'yapping',
+                owner_id: 1
+            })
+        .end(function(error, response){
+          console.log("pet id? ", response.body);
+          chai.request(server)
+          .get('/api/pets/' +response.body)
+          .end(function(err, res){
+            res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body[0].should.have.property('name');
+                res.body[0].name.should.equal('Archibald');
+                res.body[0].should.have.property('type');
+                res.body[0].type.should.equal('Dog');
+                res.body[0].should.have.property('breed');
+                res.body[0].breed.should.equal('Pomeranian');
+                res.body[0].should.have.property('picture_url');
+                res.body[0].picture_url.should.equal('https://upload.wikimedia.org/wikipedia/commons/c/ca/Pomeranian.JPG');
+                res.body[0].should.have.property('weight');
+                res.body[0].weight.should.equal('8.00');
+                res.body[0].should.have.property('allergies');
+                res.body[0].allergies.should.equal('Everything');
+                res.body[0].should.have.property('illnesses');
+                res.body[0].illnesses.should.equal('Everyting');
+                res.body[0].should.have.property('last_vet_visit');
+                res.body[0].last_vet_visit.should.equal('2016-03-12T00:00:00.000Z')
+                res.body[0].should.have.property('tendencies');
+                res.body[0].tendencies.should.equal('Is tiny');
+                res.body[0].should.have.property('favorites');
+                res.body[0].favorites.should.equal('yapping');
+                done();
+          });
+        });
+      });
+    }
+  );
+
   describe('Get all pets', function() {
 
         it('should get all pets', function(done) {
             chai.request(server)
-            .get('/pets')
+            .get('/api/pets')
             .end(function(err, res) {
                 res.should.have.status(200);
                 res.should.be.json;
@@ -43,13 +95,13 @@ describe('Pet API routes', function() {
                 res.body[0].should.have.property('picture_url');
                 res.body[0].picture_url.should.equal('http://ichef-1.bbci.co.uk/news/660/media/images/58682000/jpg/_58682918_wild_dog_screz.jpg')
                 res.body[0].should.have.property('weight');
-                res.body[0].weight.should.equal('100');
+                res.body[0].weight.should.equal('100.00');
                 res.body[0].should.have.property('allergies');
                 res.body[0].allergies.should.equal('none');
                 res.body[0].should.have.property('illnesses');
                 res.body[0].illnesses.should.equal('none');
                 res.body[0].should.have.property('last_vet_visit');
-                res.body[0].last_vet_visit.should.equal('Sat Mar 12 2016 22:59:15Z')
+                res.body[0].last_vet_visit.should.equal('2016-03-12T00:00:00.000Z')
                 res.body[0].should.have.property('tendencies');
                 res.body[0].tendencies.should.equal('eats');
                 res.body[0].should.have.property('favorites');
@@ -64,41 +116,98 @@ describe('Pet API routes', function() {
 
       it('should get one pet corresponding to a single id', function(done) {
           chai.request(server)
-          .get('/pets/1')
+          .get('/api/pets/1')
           .end(function(err, res) {
-              res.should.have.status(200);
-              res.should.be.json;
-              res.body.should.be.a('array');
-              res.body[0].should.have.property('name');
-              res.body[0].name.should.equal('Leopold');
-              res.body[0].should.have.property('type');
-              res.body[0].type.should.equal('Dog');
-              res.body[0].should.have.property('breed');
-              res.body[0].breed.should.equal('Dingo')
-              res.body[0].should.have.property('picture_url');
-              res.body[0].picture_url.should.equal('http://ichef-1.bbci.co.uk/news/660/media/images/58682000/jpg/_58682918_wild_dog_screz.jpg')
-              res.body[0].should.have.property('weight');
-              res.body[0].weight.should.equal('100');
-              res.body[0].should.have.property('allergies');
-              res.body[0].allergies.should.equal('none');
-              res.body[0].should.have.property('illnesses');
-              res.body[0].illnesses.should.equal('none');
-              res.body[0].should.have.property('last_vet_visit');
-              res.body[0].last_vet_visit.should.equal('Sat Mar 12 2016 22:59:15Z')
-              res.body[0].should.have.property('tendencies');
-              res.body[0].tendencies.should.equal('eats');
-              res.body[0].should.have.property('favorites');
-              res.body[0].favorites.should.equal('food');
-              done();
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body[0].should.have.property('name');
+                res.body[0].name.should.equal('Leopold');
+                res.body[0].should.have.property('type');
+                res.body[0].type.should.equal('Dog');
+                res.body[0].should.have.property('breed');
+                res.body[0].breed.should.equal('Dingo')
+                res.body[0].should.have.property('picture_url');
+                res.body[0].picture_url.should.equal('http://ichef-1.bbci.co.uk/news/660/media/images/58682000/jpg/_58682918_wild_dog_screz.jpg')
+                res.body[0].should.have.property('weight');
+                res.body[0].weight.should.equal('100.00');
+                res.body[0].should.have.property('allergies');
+                res.body[0].allergies.should.equal('none');
+                res.body[0].should.have.property('illnesses');
+                res.body[0].illnesses.should.equal('none');
+                res.body[0].should.have.property('last_vet_visit');
+                res.body[0].last_vet_visit.should.equal('2016-03-12T00:00:00.000Z')
+                res.body[0].should.have.property('tendencies');
+                res.body[0].tendencies.should.equal('eats');
+                res.body[0].should.have.property('favorites');
+                res.body[0].favorites.should.equal('food');
+                done();
               });
         });
     });
 
-    // describe('Add a single pet', function() {
-    //   it('should add a single pet into the db', function(done){
-    //     chai.request(server);
-    //     .post('/pets/')
-    //   });
-    // }
+    describe('Update a single pet', function() {
+      it('should update a single pet in the db', function(done){
+        chai.request(server)
+        .put('/api/pets/1/edit')
+        .send({
+                name: 'Archibald2',
+                type: 'Dog',
+                breed: 'Pomeranian',
+                picture_url: 'https://upload.wikimedia.org/wikipedia/commons/c/ca/Pomeranian.JPG',
+                weight: '8',
+                allergies: 'Everything',
+                illnesses: 'Everyting',
+                last_vet_visit: '2016-03-12T00:00:00.000Z',
+                tendencies: 'Is tiny',
+                favorites: 'yapping',
+                owner_id: 1
+            })
+        .end(function(error, response){
+          console.log("pet id? ", response.body);
+          chai.request(server)
+          .get('/api/pets/' +response.body)
+          .end(function(err, res){
+            res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body[0].should.have.property('name');
+                res.body[0].name.should.equal('Archibald2');
+                res.body[0].should.have.property('type');
+                res.body[0].type.should.equal('Dog');
+                res.body[0].should.have.property('breed');
+                res.body[0].breed.should.equal('Pomeranian');
+                res.body[0].should.have.property('picture_url');
+                res.body[0].picture_url.should.equal('https://upload.wikimedia.org/wikipedia/commons/c/ca/Pomeranian.JPG');
+                res.body[0].should.have.property('weight');
+                res.body[0].weight.should.equal('8.00');
+                res.body[0].should.have.property('allergies');
+                res.body[0].allergies.should.equal('Everything');
+                res.body[0].should.have.property('illnesses');
+                res.body[0].illnesses.should.equal('Everyting');
+                res.body[0].should.have.property('last_vet_visit');
+                res.body[0].last_vet_visit.should.equal('2016-03-12T00:00:00.000Z')
+                res.body[0].should.have.property('tendencies');
+                res.body[0].tendencies.should.equal('Is tiny');
+                res.body[0].should.have.property('favorites');
+                res.body[0].favorites.should.equal('yapping');
+                done();
+            });
+          });
+        });
+      }
+    );
+
+    describe('Delete a single pet', function(){
+      it('should delete a pet', function(done) {
+            chai.request(server)
+            .delete('/api/pets/1/delete')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                done();
+            });
+        });
+    });
 
 });
