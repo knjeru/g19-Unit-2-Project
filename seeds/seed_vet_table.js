1514ef94973558311e1b5baed3cb9adeb562a9de
Danny Robinson <daniel@djrobinson.me>
2016-03-16 14:57:17 -0600

    Updated tests. Added Travis

--- seeds/seed_vet_table.js
+++ seeds/seed_vet_table.js
@@ -1,6 +1,6 @@
 var seeder = require('knex-csv-seeder').seeder.seed;
 
 exports.seed = seeder({
-  table: 'vet_visits',
-  file: '../../data/vet_visit.csv'
+  table: 'vets',
+  file: './data/vets.csv'
 });
--- seeds/seed_vet_visit.js
+++ seeds/seed_vet_visit.js
@@ -1,6 +1,6 @@
 var seeder = require('knex-csv-seeder').seeder.seed;
 
 exports.seed = seeder({
-  table: 'vets',
-  file: '../../data/vets.csv'
+  table: 'vet_visits',
+  file: './data/vet_visit.csv'
 });
