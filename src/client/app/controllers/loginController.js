f171a248ccb35c0e93d95420f3ffe9b3a9cff735
Kevin Njeru <knjeru@gmail.com>
2016-03-17 09:26:36 -0600

    creating factories

--- src/client/app/controllers/loginController.js
+++ src/client/app/controllers/loginController.js
@@ -1,9 +1,8 @@
 'use strict';
 
 angular.module('petApp')
-.controller('LoginCtrl', ['$scope', '$http', '$routeParams', '$log',function($scope, $http,$routeParams,$log){
+.controller('LoginCtrl', ['$scope', '$http', '$routeParams', '$log','$location', function($scope, $http,$routeParams,$log,$location){
     console.log('LoginController sounding off');
-     $scope.hello = "Testing";
 
      $scope.register = function(){
         var data = {
@@ -16,6 +15,7 @@ angular.module('petApp')
         $http.post('/api/auth/register', data)
         .success(function(data) {
             $scope.hello = data;
+            $location.url('/pets/new')
         });
      };
 
@@ -28,63 +28,65 @@ angular.module('petApp')
         $http.post('/api/auth/login', data)
         .success(function(data) {
             $scope.hello = data;
-            // $log.info($scope.hello);
+            $log.info($scope.hello);
+            $location.url('/pets/main')
         });
      };
-     
+
      $scope.logout = function(){
          $log.info('you are logged out');
          $http.get('/api/auth/logout')
          .success(function(data) {
-             $log.info('you are logged out2')
+             $log.info('you are logged out2');
+             $location.url('/');
          });
      };
 
 
 }]);
 
-(function() {
-    document.getElementById("file_input").onchange = function(){
-        var files = document.getElementById("file_input").files;
-        var file = files[0];
-        if(file == null){
-            alert("No file selected.");
-        }
-        else{
-            get_signed_request(file);
-        }
-    };
-})();
-
-function get_signed_request(file){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/sign_s3?file_name="+file.name+"&file_type="+file.type);
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState === 4){
-            if(xhr.status === 200){
-                var response = JSON.parse(xhr.responseText);
-                upload_file(file, response.signed_request, response.url);
-            }
-            else{
-                alert("Could not get signed URL.");
-            }
-        }
-    };
-    xhr.send();
-}
-
-function upload_file(file, signed_request, url){
-    var xhr = new XMLHttpRequest();
-    xhr.open("PUT", signed_request);
-    xhr.setRequestHeader('x-amz-acl', 'public-read');
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            document.getElementById("preview").src = url;
-            document.getElementById("avatar_url").value = url;
-        }
-    };
-    xhr.onerror = function() {
-        alert("Could not upload file.");
-    };
-    xhr.send(file);
-}
+// (function() {
+//     document.getElementById("file_input").onchange = function(){
+//         var files = document.getElementById("file_input").files;
+//         var file = files[0];
+//         if(file == null){
+//             alert("No file selected.");
+//         }
+//         else{
+//             get_signed_request(file);
+//         }
+//     };
+// })();
+//
+// function get_signed_request(file){
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("GET", "/sign_s3?file_name="+file.name+"&file_type="+file.type);
+//     xhr.onreadystatechange = function(){
+//         if(xhr.readyState === 4){
+//             if(xhr.status === 200){
+//                 var response = JSON.parse(xhr.responseText);
+//                 upload_file(file, response.signed_request, response.url);
+//             }
+//             else{
+//                 alert("Could not get signed URL.");
+//             }
+//         }
+//     };
+//     xhr.send();
+// }
+//
+// function upload_file(file, signed_request, url){
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("PUT", signed_request);
+//     xhr.setRequestHeader('x-amz-acl', 'public-read');
+//     xhr.onload = function() {
+//         if (xhr.status === 200) {
+//             document.getElementById("preview").src = url;
+//             document.getElementById("avatar_url").value = url;
+//         }
+//     };
+//     xhr.onerror = function() {
+//         alert("Could not upload file.");
+//     };
+//     xhr.send(file);
+// }
--- src/client/app/controllers/petController.js
+++ src/client/app/controllers/petController.js
@@ -1,53 +1,83 @@
 'use strict';
 
 angular.module('petApp')
-.controller('GenPetCtrl', ['$scope', '$http', '$routeParams', function($scope, $http,$routeParams){
+  .controller('GenPetCtrl', ['$scope', '$routeParams', '$location', 'petFactory', function($scope, $routeParams, $location, petFactory){
     console.log('petController 1 sounding off');
 
-    $scope.step = function() {
-        return 1;
-    };
+  $scope.status;
+  $scope.pets;
+  $scope.quantity = 3;
 
-    $scope.nextQuestion = function () {
-      $scope.step++;
-    };
+  getPets();
 
-    $http.get('/api/pets')
-    .success(function(data) {
-        // then we'll attach the data to the intended $scope variable we want to use in the view
+  function getPets() {
+    petFactory.getPets()
+      .success(function (data) {
         $scope.pets = data;
-        // console.log($scope.pets);
-    });
+        console.log($scope.pets);
+      })
+      .error(function(error) {
+        $scope.status = 'Unable to load pet data: ' + error.message;
+      });
+  }
 
-    $scope.petFormData = {};
-    $scope.newPet = function () {
-        $http.post('/api/pets/new', $scope.petFormData)
-        .success(function(data) {
-            // we can set an action to the let the user know the request went through
-        });
-    };
-}])
-.controller('SinglePetCtrl', ['$scope', '$http', '$routeParams', function($scope, $http,$routeParams){
-    console.log('petController 2 sounding off');
+      $scope.petFormData = {};
+      $scope.newPet = function () {
+          petFactory.insertPet($scope.petFormData)
+            .success(function(data) {
+              $location.url('/pets/main');
+            }).error(function(error) {
+              $scope.status = 'Unable to add pet data: ' + error.message;
+            });
+          };
 
-    $http.get('/api/pets/'+ $routeParams.id)
-    .success(function(data) {
-        console.log('This should be the id: ', $routeParams.id);
-        $scope.pet = data[0];
-        console.log('here is my data: ', $scope.pet);
-    });
+      // $scope.step = function() {
+      //     return 1;
+      // };
+      //
+      // $scope.nextQuestion = function () {
+      //   $scope.step++;
+      // };
+  }])
+  .controller('SinglePetCtrl', ['$scope', '$routeParams', 'petFactory', function($scope, $routeParams, petFactory){
+      console.log('petController 2 sounding off');
 
-    $scope.updatePet = function() {
-        $http.put('/api/pets/'+$routeParams.id+'/edit', $scope.petFormData)
-        .success(function(data) {
-            // we can set an action to the let the user know the request went through
-        });
-    }
+      $scope.id = $routeParams.id;
 
-    $scope.removePet = function() {
-        $http.delete('/api/pets/'+$routeParams.id+'/delete')
-        .success(function(data) {
-            // we can set an action to the let the user know the request went through
-        });
-    }
-}]);
+      getPet();
+
+      function getPet() {
+        console.log('inside get pet');
+        petFactory.getPet($scope.id)
+          .success(function(data) {
+            $scope.pet = data[0]
+          }).error(function(error) {
+            $scope.status = 'Unable to load pet data: ' + error.message;
+          });
+      };
+
+      function createPet() {
+        petFactory.insertPet() {
+          .success(function(data) {
+
+          }).error(function(data) {
+
+          })
+
+        }
+      }
+
+      $scope.updatePet = function() {
+          $http.put('/api/pets/'+$routeParams.id+'/edit', $scope.petFormData)
+          .success(function(data) {
+              // we can set an action to the let the user know the request went through
+          });
+      }
+
+      $scope.removePet = function() {
+          $http.delete('/api/pets/'+$routeParams.id+'/delete')
+          .success(function(data) {
+              // we can set an action to the let the user know the request went through
+          });
+      }
+  }]);
